e3f4167436d22c5ef61154062e83c30493a6857d
jione16 <onebornawkward@gmail.com>
2020-09-08 17:29:28 +0700

    plastic

---
+++ app.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+from router.bot import router as bot_router
+
+app = FastAPI()
+
+app.include_router(router=bot_router)
+
+if __name__ == "__main__":
+    uvicorn.run('app:app', host='localhost', port=1616,
+                log_level='info', reload=True)
